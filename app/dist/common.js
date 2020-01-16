88a497adc7e031c14ae56d2b2c5226de8b5ee85d
Daniil <komarama21@gmail.com>
2020-01-17 00:13:25 +0300

    Added transpilled JS file

---
+++ app/dist/common.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function addTask() {
+    var taskContainer = document.querySelector('.taskContainer');
+    var newElem = document.createElement('div');
+    newElem.className = "task";
+    var mainInput = document.querySelector('.mainInput');
+    newElem.innerHTML = mainInput.value;
+    taskContainer.prepend(newElem);
+    mainInput.value = "";
+}
+
+var targetButton = document.querySelector('.button');
+targetButton.addEventListener("click", addTask);
+
+var taskContainer = document.querySelector('.taskContainer');
+taskContainer.onclick = function (event) {
+    event.target.remove();
+};
