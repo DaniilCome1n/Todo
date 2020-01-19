af3362c39331cc0985978203ffc635eae17add2d
Daniil <komarama21@gmail.com>
2020-01-19 22:18:46 +0300

    Added ability to use localStorage

--- app/dist/common.js
+++ app/dist/common.js
@@ -1,23 +1,33 @@
 'use strict';
 
-function createElem(elem, container) {
-    container.prepend(elem);
-}
-
-function addTask() {
-    var taskContainer = document.querySelector('.taskContainer');
-    var newElem = document.createElement('div');
-    newElem.className = "task";
-    var mainInput = document.querySelector('.mainInput');
-    newElem.innerHTML = mainInput.value;
-    createElem(newElem, taskContainer);
-    mainInput.value = "";
-}
+var taskContainer = document.querySelector('.taskContainer');
 
 var targetButton = document.querySelector('.button');
 targetButton.addEventListener("click", addTask);
 
-var taskContainer = document.querySelector('.taskContainer');
+function createElem(elem, container) {
+    container.prepend(elem);
+}
+
+function addListToLocal() {
+    localStorage.setItem("tasks", taskContainer.innerHTML);
+}
+
+function addTask() {
+    var mainInput = document.querySelector('.mainInput');
+    var newElem = document.createElement('div');
+    newElem.className = "task";
+    newElem.innerHTML = mainInput.value;
+    createElem(newElem, taskContainer);
+    mainInput.value = "";
+    addListToLocal();
+}
+
 taskContainer.onclick = function (e) {
     e.target.remove();
+    addListToLocal();
 };
+
+if (localStorage.getItem("tasks")) {
+    taskContainer.innerHTML = localStorage.getItem("tasks");
+}
