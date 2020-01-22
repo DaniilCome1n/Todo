b99344e3771f8bc926a98ede422c0a4c08400972
Daniil <komarama21@gmail.com>
2020-01-22 12:28:28 +0300

    Add another way ti use localStorage without using visual data

--- app/dist/common.js
+++ app/dist/common.js
@@ -1,37 +1,82 @@
 'use strict';
 
 var taskContainer = document.querySelector('.taskContainer');
-
+var mainInput = document.querySelector('.mainInput');
 var targetButton = document.querySelector('.button');
-targetButton.addEventListener("click", addTask);
 
-function createElem(elem, container) {
-    container.prepend(elem);
+function getInitialTasks() {
+    //Инициализация массива объектов задач
+    if (localStorage.getItem("tasks")) {
+        return JSON.parse(localStorage.getItem("tasks")); //Если в localStorage есть информация
+    } else {
+        return []; //Если страница загружается впервые
+    }
 }
+
+var tasks = getInitialTasks();
+tasks.forEach(addTaskToDOM); //Построение DOM-дерева на основе tasks
+
+targetButton.addEventListener("click", createTaskElem); //Вешаем обработчик на кнопку "ADD TASK"
+
+document.addEventListener("keydown", function (event) {
+    //Обработчик нажатой клавиши "Enter"
+    if (event.key == 'Enter') {
+        createTaskElem();
+    }
+});
 
 function addListToLocal() {
-    localStorage.setItem("tasks", taskContainer.innerHTML);
+    //добавление массива задач в localStorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-function addTask() {
-    var mainInput = document.querySelector('.mainInput');
-    var newElem = document.createElement('li');
-    newElem.className = "task";
-    newElem.innerHTML = mainInput.value;
-    if (mainInput.value == "") {
-        console.log("Enter task");
-    } else {
-        createElem(newElem, taskContainer);
-    }
+function createTaskValue(task) {
+    //Создание текста задачи для объекта task
+    task.value = mainInput.value;
+}
+function createTaskID(task) {
+    //Генерация и cсоздание ID задачи для объекта task
+    task.id = new Date();
+}
+
+function createTaskObj(task) {
+    //Добавление текста задачи и ID в объект task
+    createTaskValue(task);
+    createTaskID(task);
+}
+
+function addTask(task) {
+    //Добавление объекта задачи в массив задач
+    tasks.push(task);
+}
+
+function addTaskToDOM(task) {
+    //Создание элемента DOM на основе объекта задачи
+    var elem = document.createElement('li');
+    elem.className = 'task';
+    elem.innerHTML = task.value;
+    elem.setAttribute('id', task.id);
+    taskContainer.append(elem);
     mainInput.value = "";
-    addListToLocal();
+}
+
+function createTaskElem() {
+    var task = {
+        id: "",
+        value: ""
+    };
+    createTaskObj(task);
+    addTask(task);
+    addTaskToDOM(task);
+    addListToLocal(); //Обновление localStorage
 }
 
 taskContainer.onclick = function (e) {
-    e.target.remove();
-    addListToLocal();
+    //Вешаем обработчик на элемент задачи для его удаления                                       
+    var delTaskIndex = tasks.findIndex(function (item) {
+        return item.id == e.target.id;
+    });
+    tasks.splice(delTaskIndex, 1); //Удаление объекта задачи из массива задач
+    e.target.remove(); //Удаление элемента DOM
+    addListToLocal(); //Обновление localStorage
 };
-
-if (localStorage.getItem("tasks")) {
-    taskContainer.innerHTML = localStorage.getItem("tasks");
-}
--- app/js/common.js
+++ app/js/common.js
@@ -1,42 +1,72 @@
 let taskContainer = document.querySelector('.taskContainer');
+let mainInput = document.querySelector('.mainInput');
+let targetButton = document.querySelector('.button');
 
-const targetButton = document.querySelector('.button');
-targetButton.addEventListener("click",addTask);
 
-function createElem(elem,container){
-    container.prepend(elem);
-}
-
-function addListToLocal(){
-    localStorage.setItem("tasks", taskContainer.innerHTML);
-}
-
-function addTask(){
-    let mainInput = document.querySelector('.mainInput');
-    let newElem = document.createElement('li');
-    newElem.className = "task";
-    newElem.innerHTML = mainInput.value;
-        if(mainInput.value == ""){
-            console.log("Enter task");
+function getInitialTasks(){                                 //Инициализация массива объектов задач
+        if(localStorage.getItem("tasks")){                  
+            return JSON.parse(localStorage.getItem("tasks"))//Если в localStorage есть информация
         }else{
-            createElem(newElem,taskContainer);
+            return [];                                      //Если страница загружается впервые
         }
+    }
+
+const tasks = getInitialTasks();
+tasks.forEach(addTaskToDOM);                                //Построение DOM-дерева на основе tasks
+
+targetButton.addEventListener("click",createTaskElem);      //Вешаем обработчик на кнопку "ADD TASK"
+
+document.addEventListener("keydown",(event)=>{              //Обработчик нажатой клавиши "Enter"
+    if (event.key == 'Enter'){
+        createTaskElem();
+    }
+}); 
+
+function addListToLocal(){                                  //добавление массива задач в localStorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+function createTaskValue(task){                             //Создание текста задачи для объекта task
+    task.value = mainInput.value;
+    }
+function createTaskID(task){                                //Генерация и cсоздание ID задачи для объекта task
+    task.id = new Date() ;
+    }
+
+function createTaskObj(task){                               //Добавление текста задачи и ID в объект task
+    createTaskValue(task);
+    createTaskID(task);
+    }
+
+function addTask(task){                                     //Добавление объекта задачи в массив задач
+    tasks.push(task);
+    }
+
+function addTaskToDOM(task){                                //Создание элемента DOM на основе объекта задачи
+    let elem = document.createElement('li');
+    elem.className = 'task';
+    elem.innerHTML=task.value;
+    elem.setAttribute('id', task.id);
+    taskContainer.append(elem);
     mainInput.value = "";
-    addListToLocal();
-}
+    }
 
-taskContainer.onclick =(e)=>{
-    e.target.remove();
-    addListToLocal();
-}
+function createTaskElem(){
+    let task = {
+        id:"",
+        value:""
+            }
+    createTaskObj(task);
+    addTask(task);
+    addTaskToDOM(task);
+    addListToLocal();                                       //Обновление localStorage
+    }
 
-if(localStorage.getItem("tasks")){
-    taskContainer.innerHTML = localStorage.getItem("tasks");
-}
-
-
-
-
-
+taskContainer.onclick =(e)=>{                               //Вешаем обработчик на элемент задачи для его удаления                                       
+    let delTaskIndex = tasks.findIndex(item => item.id == e.target.id);
+    tasks.splice(delTaskIndex,1);                           //Удаление объекта задачи из массива задач
+    e.target.remove();                                      //Удаление элемента DOM
+    addListToLocal();                                       //Обновление localStorage
+    }
 
 
